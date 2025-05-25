49b6b5ca105356def491f59677d0e6e6a7be1c83
Lena_Bondar <lencha.2005@gmail.com>
2025-05-25 19:18:27 +0300

    add authConfig

---
+++ src/lib/auth/config.ts
@@ -0,0 +1,65 @@
+import type { AuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { AppUser } from '@/types';
+
+export const authConfig: AuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        try {
+          const res = await fetch(
+            'https://vocab-builder-backend.p.goit.global/api/users/signin',
+            {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({
+                email: credentials?.email,
+                password: credentials?.password,
+              }),
+            }
+          );
+
+          if (!res.ok) return null;
+
+          const user: AppUser = await res.json();
+
+          return {
+            id: user.email,
+            name: user.name,
+            email: user.email,
+            token: user.token,
+          };
+        } catch (error) {
+          console.error('Authorize error:', error);
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token?.user) {
+        session.user = token.user as AppUser;
+      }
+      return session;
+    },
+  },
+};
