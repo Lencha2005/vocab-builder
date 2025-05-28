862075104d3d4e18b3a6069fe78556777ff53fb7
Lena_Bondar <lencha.2005@gmail.com>
2025-05-28 19:18:01 +0300

    Update page.tsx

--- src/app/recommend/page.tsx
+++ src/app/recommend/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from 'react';
 import Recommend from './recommend';
+import Loader from '@/components/ui/loader';
 
 export default function Page() {
-  return <Recommend />;
+  return (
+    <Suspense fallback={<Loader />}>
+      <Recommend />
+    </Suspense>
+  );
 }
